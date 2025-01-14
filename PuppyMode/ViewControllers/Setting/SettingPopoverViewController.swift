6efa59e1555965728deae6b846d3083ce4e01ffa
Rudy-009 <rofxnaos@gmail.com>
2025-01-14 15:51:14 +0900

    feat: feat: add tap outside modal to dismiss

--- PuppyMode/ViewControllers/Setting/SettingPopoverViewController.swift
+++ PuppyMode/ViewControllers/Setting/SettingPopoverViewController.swift
@@ -8,13 +8,16 @@
 import UIKit
 
 class SettingPopoverViewController: UIViewController {
-
+    
     let popover = PolicyOrTermPopoverView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view = popover
         popover.confirmButton.addTarget(self, action: #selector(popView), for: .touchUpInside)
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleGrayAreaTap(_:)))
+        // 팝오버의 배경뷰에 제스처 추가
+        view.addGestureRecognizer(tapGesture)
     }
     
     @objc
@@ -25,5 +28,12 @@ class SettingPopoverViewController: UIViewController {
     public func configurePopoverView(title: String, content: String) {
         popover.configure(title: title, content: content)
     }
-
+    
+    @objc func handleGrayAreaTap(_ gesture: UITapGestureRecognizer) {
+        let location = gesture.location(in: view)
+        if gesture.view == view {
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+    
 }
