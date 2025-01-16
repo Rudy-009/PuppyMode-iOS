359f3335765f9e5a88495d5ac2c915f28e8b1b4c
Rudy-009 <rofxnaos@gmail.com>
2025-01-16 20:27:26 +0900

    feat: Add  Kakao APP Key in Security.xcconfig file

---
+++ PuppyMode/Models/Bundle.swift
@@ -0,0 +1,14 @@
+//
+//  Bundle.swift
+//  PuppyMode
+//
+//  Created by 이승준 on 1/16/25.
+//
+
+import Foundation
+
+extension Bundle {
+    var kakaoAppKey: String? {
+        return infoDictionary?["Kakao_APP_KEY"] as? String
+    }
+}
