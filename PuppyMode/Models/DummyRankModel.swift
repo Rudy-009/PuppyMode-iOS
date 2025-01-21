03d83a59c4b1aa21d73058fd6322068fceba8fac
Rudy-009 <rofxnaos@gmail.com>
2025-01-21 15:27:26 +0900

    feat: Add SocialTableView

---
+++ PuppyMode/Models/DummyRankModel.swift
@@ -0,0 +1,63 @@
+//
+//  DummyRankModel.swift
+//  PuppyMode
+//
+//  Created by 이승준 on 1/21/25.
+//
+
+import UIKit
+
+class DummyRankModel {
+    
+    static let allData: [RankCell] = [
+        RankCell(name: "김민준", characterName: "골든리트리버", characterLevel: 3),
+        RankCell(name: "이서연", characterName: "비숑프리제", characterLevel: 4),
+        RankCell(name: "박지훈", characterName: "포메라니안", characterLevel: 2),
+        RankCell(name: "최수아", characterName: "시바견", characterLevel: 5),
+        RankCell(name: "정도현", characterName: "웰시코기", characterLevel: 3),
+        RankCell(name: "강하은", characterName: "말티즈", characterLevel: 1),
+        RankCell(name: "윤서준", characterName: "치와와", characterLevel: 4),
+        RankCell(name: "임지아", characterName: "푸들", characterLevel: 2),
+        RankCell(name: "한유진", characterName: "시츄", characterLevel: 5),
+        RankCell(name: "송민서", characterName: "닥스훈트", characterLevel: 3),
+        RankCell(name: "오지원", characterName: "허스키", characterLevel: 4),
+        RankCell(name: "신예준", characterName: "진돗개", characterLevel: 2),
+        RankCell(name: "황서윤", characterName: "불독", characterLevel: 5),
+        RankCell(name: "조현우", characterName: "사모예드", characterLevel: 1),
+        RankCell(name: "백수민", characterName: "비글", characterLevel: 3),
+        RankCell(name: "류하린", characterName: "달마시안", characterLevel: 4),
+        RankCell(name: "남도윤", characterName: "차우차우", characterLevel: 2),
+        RankCell(name: "문서현", characterName: "보더콜리", characterLevel: 5),
+        RankCell(name: "구민재", characterName: "요크셔테리어", characterLevel: 3),
+        RankCell(name: "안지유", characterName: "래브라도리트리버", characterLevel: 1),
+    ]
+    
+    static let friendData: [RankCell] = [
+        RankCell(name: "오지원", characterName: "허스키", characterLevel: 4),
+        RankCell(name: "신예준", characterName: "진돗개", characterLevel: 2),
+        RankCell(name: "황서윤", characterName: "불독", characterLevel: 5),
+        RankCell(name: "조현우", characterName: "사모예드", characterLevel: 1),
+        RankCell(name: "백수민", characterName: "비글", characterLevel: 3),
+        RankCell(name: "류하린", characterName: "달마시안", characterLevel: 4),
+        RankCell(name: "남도윤", characterName: "차우차우", characterLevel: 2),
+        RankCell(name: "문서현", characterName: "보더콜리", characterLevel: 5),
+        RankCell(name: "구민재", characterName: "요크셔테리어", characterLevel: 3),
+        RankCell(name: "안지유", characterName: "래브라도리트리버", characterLevel: 1),
+        RankCell(name: "김민준", characterName: "골든리트리버", characterLevel: 3),
+        RankCell(name: "이서연", characterName: "비숑프리제", characterLevel: 4),
+        RankCell(name: "박지훈", characterName: "포메라니안", characterLevel: 2),
+        RankCell(name: "최수아", characterName: "시바견", characterLevel: 5),
+        RankCell(name: "정도현", characterName: "웰시코기", characterLevel: 3),
+        RankCell(name: "강하은", characterName: "말티즈", characterLevel: 1),
+        RankCell(name: "윤서준", characterName: "치와와", characterLevel: 4),
+        RankCell(name: "임지아", characterName: "푸들", characterLevel: 2),
+        RankCell(name: "한유진", characterName: "시츄", characterLevel: 5),
+        RankCell(name: "송민서", characterName: "닥스훈트", characterLevel: 3),
+    ]
+}
+
+struct RankCell {
+    var name: String
+    var characterName: String
+    var characterLevel: Int
+}
--- PuppyMode/ViewControllers/SocialViewController.swift
+++ PuppyMode/ViewControllers/SocialViewController.swift
@@ -15,6 +15,8 @@ class SocialViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = UIColor(red: 251/255, green: 251/255, blue: 251/255, alpha: 1)
         self.view = socialView
+        socialView.rankingTableView.delegate = self
+        socialView.rankingTableView.dataSource = self
         setupAction()
     }
     
@@ -25,14 +27,35 @@ class SocialViewController: UIViewController {
     @objc
     private func segmentedControlValueChanged(segment: UISegmentedControl) {
         if segment.selectedSegmentIndex == 0 {
-            socialView.label01.isHidden = false
-            socialView.label02.isHidden = true
+            
         } else {
-            socialView.label01.isHidden = true
-            socialView.label02.isHidden = false
+            
         }
     }
 
+}
+
+extension SocialViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return DummyRankModel.allData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let data = DummyRankModel.allData[indexPath.row]
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: RankingTableViewCell.identifier,
+            for: indexPath) as? RankingTableViewCell
+        else {
+            return UITableViewCell()
+        }
+        cell.configure(index: indexPath.row, rankCell: data)
+        if indexPath.row < 3 {
+            cell.addTrophyComponent(rank: Rank(rawValue: indexPath.row + 1) ?? Rank.first)
+        }
+        return cell
+    }
+    
 }
 
 #Preview{
