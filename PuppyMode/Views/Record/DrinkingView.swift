cca6e884600628d10d6f477199698526fe8a8695
alwn8918 <alwn8918@gmail.com>
2025-01-22 07:41:24 +0900

    Feat: 주량 기록 페이지 버튼 생성 (#14)

--- PuppyMode/Views/Record/DrinkingView.swift
+++ PuppyMode/Views/Record/DrinkingView.swift
@@ -21,6 +21,34 @@ class DrinkingView: UIView {
         $0.font = UIFont(name: "NotoSansKR-Medium", size: 20)
     }
     
+    // 추가 버튼
+    private let plusButton = UIButton().then {
+        $0.setImage(.iconPlus, for: .normal)
+        $0.backgroundColor = .white
+        
+        $0.layer.borderWidth = 1
+        $0.layer.borderColor = UIColor(red: 0.873, green: 0.873, blue: 0.873, alpha: 1).cgColor
+        $0.layer.cornerRadius = 18
+    }
+    
+    // 입력 완료 버튼
+    public let completeButton = UIButton().then {
+        $0.backgroundColor = .main
+
+        $0.setTitle("입력 완료", for: .normal)
+        $0.setTitleColor(UIColor(red: 0.235, green: 0.235, blue: 0.235, alpha: 1), for: .normal)
+        $0.titleLabel?.font = UIFont(name: "NotoSansKR-Medium", size: 20)
+
+        $0.layer.borderWidth = 1
+        $0.layer.borderColor = UIColor(red: 212/255, green: 212/255, blue: 212/255, alpha: 1).cgColor
+        $0.layer.cornerRadius = 10
+        
+        $0.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
+        $0.layer.shadowOpacity = 1
+        $0.layer.shadowRadius = 2
+        $0.layer.shadowOffset = CGSize(width: 0, height: 4)
+    }
+    
     // MARK: - init
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -37,7 +65,9 @@ class DrinkingView: UIView {
     private func setView() {
         [
             backButton,
-            titleLabel
+            titleLabel,
+            plusButton,
+            completeButton
         ].forEach {
             addSubview($0)
         }
@@ -52,6 +82,19 @@ class DrinkingView: UIView {
         titleLabel.snp.makeConstraints {
             $0.centerX.equalToSuperview()
             $0.centerY.equalTo(backButton)
+        }
+        
+        plusButton.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(30)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(100)
+            $0.height.equalTo(36)
+        }
+        
+        completeButton.snp.makeConstraints {
+            $0.bottom.equalToSuperview().offset(-47)
+            $0.horizontalEdges.equalToSuperview().inset(16)
+            $0.height.equalTo(60)
         }
     }
 
