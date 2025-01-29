69c697f220240dab39cf8902a03380c4721e790b
Rudy-009 <rofxnaos@gmail.com>
2025-01-29 20:58:03 +0900

    feat: implement post notification

--- PuppyMode/Responses/NotificationResponse.swift
+++ PuppyMode/Responses/NotificationResponse.swift
@@ -17,3 +17,33 @@ struct NotificationsResponse: Codable {
 struct ReceiveNotificationsResponse: Codable {
     let receiveNotifications: Bool
 }
+
+struct NotificationPostBody: Codable {
+    let token: String
+    let title: String
+    let body: String
+    let image: String?
+}
+
+struct NotificationPostResponse: Codable {
+    let isSuccess: Bool
+    let code: String
+    let message: String
+    let result: NotificationResult?
+}
+
+struct NotificationResult: Codable {
+    let validateOnly: Bool
+    let message: MessageContent
+}
+
+struct MessageContent: Codable {
+    let notification: NotificationContent
+    let token: String
+}
+
+struct NotificationContent: Codable {
+    let title: String
+    let body: String
+    let image: String?
+}
