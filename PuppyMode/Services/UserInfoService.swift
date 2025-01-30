dfc7ae657edf65570b8c9f464ede9177e6f9f17a
Rudy-009 <rofxnaos@gmail.com>
2025-01-30 18:07:57 +0900

    rename: Rename addUserInfo fucntion

--- PuppyMode/Services/UserInfoService.swift
+++ PuppyMode/Services/UserInfoService.swift
@@ -9,14 +9,14 @@ import Foundation
 
 class UserInfoService {
     
-    static func addUserInfo(userInfo: LoginResult) -> Bool {
+    static func addUserInfoToKeychainService(userInfo: LoginResult) -> Bool {
         return KeychainService.add(key: UserInfoKey.jwt.rawValue, value: userInfo.jwt) &&
         KeychainService.add(key: UserInfoKey.userId.rawValue, value: String(userInfo.userInfo.userId)) &&
         KeychainService.add(key: UserInfoKey.username.rawValue, value: userInfo.userInfo.username) &&
         KeychainService.add(key: UserInfoKey.email.rawValue, value: userInfo.userInfo.email)
     }
     
-    static func deleteAllUserInfo() -> Bool {
+    static func deleteAllUserInfoFromKeychainService() -> Bool {
         return KeychainService.delete(key: UserInfoKey.jwt.rawValue) &&
         KeychainService.delete(key: UserInfoKey.userId.rawValue) &&
         KeychainService.delete(key: UserInfoKey.username.rawValue) &&
