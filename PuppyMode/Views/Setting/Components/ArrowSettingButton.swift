ba744edc12e470c52c4a92ac84f18eb0c32e51cf
Rudy-009 <rofxnaos@gmail.com>
2025-02-13 02:44:33 +0900

    feat: resize pop button touch area

--- PuppyMode/Views/Setting/Components/ArrowSettingButton.swift
+++ PuppyMode/Views/Setting/Components/ArrowSettingButton.swift
@@ -1,28 +1,27 @@
 //
-//  alarmSettingView.swift
+//  SettingCustomButton.swift
 //  PuppyMode
 //
-//  Created by 이승준 on 1/12/25.
+//  Created by 이승준 on 1/11/25.
 //
 
 import UIKit
 
-class AlarmSettingView: UIView {
+class ArrowSettingButton: UIButton {
     
-    private lazy var notificationTitleLabel = UILabel().then { label in
+    private lazy var title = UILabel().then { label in
         label.textColor = UIColor(red: 0.235, green: 0.235, blue: 0.235, alpha: 1)
         label.font = UIFont(name: "Pretendard-Regular", size: 18)
-        label.text = "알림"
     }
     
-    public lazy var toggleView = UISwitch().then { toggle in
-        toggle.onTintColor = .main
+    private lazy var biggerImage = UIImageView().then { image in
+        image.image = UIImage(named: "Bigger")
     }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.backgroundColor = .white
-        self.addComponents()
+        addComponents()
     }
     
     required init?(coder: NSCoder) {
@@ -34,20 +33,24 @@ class AlarmSettingView: UIView {
             make.height.equalTo(60)
         }
         
-        self.addSubview(notificationTitleLabel)
-        self.addSubview(toggleView)
+        self.addSubview(title)
+        self.addSubview(biggerImage)
         
-        notificationTitleLabel.snp.makeConstraints { make in
-            make.centerY.equalTo(self.snp.centerY)
+        title.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
             make.leading.equalToSuperview().offset(36)
         }
         
-        toggleView.snp.makeConstraints { make in
-            make.centerY.equalTo(self.snp.centerY)
+        biggerImage.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
             make.trailing.equalToSuperview().offset(-36)
+            make.height.equalTo(8)
+            make.width.equalTo(4)
         }
     }
     
+    public func setTitle(for title: String) {
+        self.title.text = title
+    }
+    
 }
-
-
--- PuppyMode/Views/Setting/Components/PopButton.swift
+++ PuppyMode/Views/Setting/Components/PopButton.swift
@@ -5,4 +5,32 @@
 //  Created by 이승준 on 2/13/25.
 //
 
-import Foundation
+import UIKit
+
+class PopButton: UIButton {
+    
+    private lazy var popImage = UIImageView().then {
+        $0.contentMode = .scaleAspectFit
+        $0.image = .back
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = UIColor(red: 251/255, green: 251/255, blue: 251/255, alpha: 1)
+        self.addComponents()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addComponents() {
+        self.addSubview(popImage)
+        
+        popImage.snp.makeConstraints { make in
+            make.height.equalTo(20)
+            make.width.equalTo(13)
+            make.center.equalToSuperview()
+        }
+    }
+}
